e96c7dfbe97c423b47db44efc18d63376367441d
Yuqiao Gong <gyq123@sjtu.edu.cn>
2025-01-01 12:54:42 +0800

    updata analysis

--- analysis/plot/PCA_dim.R
+++ analysis/plot/PCA_dim.R
@@ -2,6 +2,8 @@ library(readxl)
 library(reshape2)
 library(ggplot2)
 
+# real data
+
 PC_res <- read_excel("data/PCA_dim.xlsx",sheet=1)
 
 ggplot(PC_res,aes(x=PCs, y=ARI))+
@@ -16,15 +18,63 @@ ggplot(PC_res,aes(x=PCs, y=ARI))+
 ggsave(filename = 'PCs_res.svg',width = 6, height = 4)
 
 
-PC_var <- read_excel("data/PCA_dim.xlsx",sheet=2)
+PC_var <- read_excel("data/PCA_dim.xlsx",sheet=5)
 
 ggplot(PC_var,aes(x=PCs, y=var))+
   geom_line(linewidth=1) +
   labs(x='Number of PCs',y='Variance Explained')+
-  # scale_y_continuous(limits = c(0,NA))+
   theme_classic()+
   theme(axis.title = element_text(size=15,colour = 'black'),
         axis.text = element_text(size=13,colour = 'black'),
         strip.text = element_text(size = 14))
 ggsave(filename = 'PCs_var.svg',width = 6, height = 4)
 
+
+# simulation data
+
+PC_res1 <- read_excel("data/PCA_dim.xlsx",sheet=2)
+PC_res1$scale <- '80K'
+PC_res2 <- read_excel("data/PCA_dim.xlsx",sheet=3)
+PC_res2$scale <- '360K'
+PC_res3 <- read_excel("data/PCA_dim.xlsx",sheet=4)
+PC_res3$scale <- '640K'
+PC_res <- rbind(PC_res1,PC_res2)
+PC_res <- rbind(PC_res,PC_res3)
+PC_res$scale <- factor(PC_res$scale,levels = c('80K','360K','640K'))
+
+ggplot(PC_res,aes(x=PCs, y=ARI,colour = scale))+
+  geom_line(linewidth=1,alpha=0.8) +
+  geom_point(size=2,alpha=0.8)+
+  labs(x='Number of PCs',y='Performance (ARI)')+
+  scale_y_continuous(limits = c(0,NA))+
+  theme_classic()+
+  theme(axis.title = element_text(size=15,colour = 'black'),
+        axis.text = element_text(size=13,colour = 'black'),
+        strip.text = element_text(size = 14),
+        legend.title = element_text(size=15,colour = 'black'),
+        legend.text = element_text(size=13,colour = 'black'),
+        legend.position = c(0.9,0.2))
+ggsave(filename = 'PCs_res_simu.svg',width = 6, height = 4)
+
+
+PC_var1 <- read_excel("data/PCA_dim.xlsx",sheet=6)
+PC_var1$scale <- '80K'
+PC_var2 <- read_excel("data/PCA_dim.xlsx",sheet=7)
+PC_var2$scale <- '360K'
+PC_var3 <- read_excel("data/PCA_dim.xlsx",sheet=8)
+PC_var3$scale <- '640K'
+PC_var <- rbind(PC_var1,PC_var2)
+PC_var <- rbind(PC_var,PC_var3)
+PC_var$scale <- factor(PC_var$scale,levels = c('80K','360K','640K'))
+
+ggplot(PC_var,aes(x=PCs, y=var,colour = scale))+
+  geom_line(linewidth=1,alpha=0.8) +
+  labs(x='Number of PCs',y='Variance Explained')+
+  theme_classic()+
+  theme(axis.title = element_text(size=15,colour = 'black'),
+        axis.text = element_text(size=13,colour = 'black'),
+        strip.text = element_text(size = 14),
+        legend.title = element_text(size=15,colour = 'black'),
+        legend.text = element_text(size=13,colour = 'black'),
+        legend.position = c(0.9,0.8))
+ggsave(filename = 'PCs_var_simu.svg',width = 6, height = 4)
--- analysis/plot/num_patches.R
+++ analysis/plot/num_patches.R
@@ -2,11 +2,24 @@ library(readxl)
 library(reshape2)
 library(ggplot2)
 
-resdf <- read_excel("data/num_patches.xlsx")[,-1]
+resdf1 <- read_excel("data/num_patches.xlsx",sheet = 1)[,-1]
+res1 <- melt(resdf1,id.vars = 'Number of spots per patch')
+colnames(res1) <- c("Number of spots per patch", "Metric","Measurement")
+res1$scale <- '80k'
 
-res <- melt(resdf,id.vars = 'Number of spots per patch')
-res$`Number of spots per patch` <- log10(res$`Number of spots per patch`)
-colnames(res) <- c("Number of spots per patch (log10)", "Metric","Measurement")
+resdf2 <- read_excel("data/num_patches.xlsx",sheet = 2)[,-1]
+res2 <- melt(resdf2,id.vars = 'Number of spots per patch')
+colnames(res2) <- c("Number of spots per patch", "Metric","Measurement")
+res2$scale <- '360k'
+
+resdf3 <- read_excel("data/num_patches.xlsx",sheet = 3)[,-1]
+res3 <- melt(resdf3,id.vars = 'Number of spots per patch')
+colnames(res3) <- c("Number of spots per patch", "Metric","Measurement")
+res3$scale <- '640k'
+
+res <- rbind(res1,res2)
+res <- rbind(res,res3)
+res$scale <- factor(res$scale,levels = c('80k','360k','640k'))
 
 # in paper
 ggplot(res,aes(x=`Number of spots per patch (log10)`, y=Measurement, colour=Metric))+
@@ -23,16 +36,17 @@ ggplot(res,aes(x=`Number of spots per patch (log10)`, y=Measurement, colour=Metr
 ggsave(filename = 'num_patches.svg',width = 3.8, height = 9)
 
 # in response
-ggplot(res,aes(x=`Number of spots per patch (log10)`, y=Measurement, colour=Metric))+
+ggplot(res,aes(x=`Number of spots per patch`, y=Measurement, colour=scale))+
   geom_line(linewidth=1) +
   geom_point(size=2)+
-  scale_x_continuous(limits = c(NA,4.5))+
+  scale_x_continuous(breaks = c(0,5000,10000,15000,20000,25000))+
   theme_classic()+
   theme(axis.title = element_text(size=15,colour = 'black'),
         axis.title.y = element_blank(),
         axis.text = element_text(size=13,colour = 'black'),
-        strip.text = element_text(size = 14))+
-  guides(color = 'none') +
+        strip.text = element_text(size = 14),
+        legend.title = element_text(size=15,colour = 'black'),
+        legend.text = element_text(size=13,colour = 'black'))+
   facet_wrap(~ Metric, scales = "free_y")
-ggsave(filename = 'num_patches_response.svg',width = 9, height = 6)
+ggsave(filename = 'num_patches_response.svg',width = 11, height = 7)
 
